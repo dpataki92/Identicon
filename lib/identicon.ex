2fc5bbfb63cf1ca95c42378bd6ef2cef9628a5e4
dpataki92 <ptamasdaniel92@gmail.com>
2021-04-25 19:54:11 +0200

    add image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,8 +5,9 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input) # creates hash from string using md5 algorithm
+    hex = :crypto.hash(:md5, input) # creates hash from string using md5 algorithm
     |> :binary.bin_to_list # produces a list of numbers from hash
-  end
 
+    %Identicon.Image{hex: hex}
+  end
 end
